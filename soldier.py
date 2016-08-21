e11c97276080e0164d81cc8431ac3eb746f446e3
Sricharan R <sricharanravi@gmail.com>
2016-08-21 09:13:09 +0530

    Add files via upload

---
+++ soldier.py
@@ -0,0 +1,15 @@
+"""
+Input = 3 17 4
+
+Output = 13
+"""
+k, n, w = raw_input().split(' ')
+k = int(k)
+n = int(n)
+w = int(w)
+calculated_sum = (((((w * (w - 1)) / 2) + w) * k) )
+if calculated_sum < n:
+	amount_the_soldier_has_to_borrow = 0
+else:
+	amount_the_soldier_has_to_borrow = calculated_sum - n
+print amount_the_soldier_has_to_borrow
